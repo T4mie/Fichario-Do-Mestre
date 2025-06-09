bb5a5efb28afbb58d13069cc3839521018227f57
nene <belavezzu@gmail.com>
2025-06-09 18:58:40 -0300

    Manter usuário
    Agora, caso o usuário já esteja logado ao clicar novamente no botão de login, ele é direcionado automaticamente para a aba de user

--- src/components/headers/Mainheader.tsx
+++ src/components/headers/Mainheader.tsx
@@ -1,16 +1,40 @@
-import {NavLink } from "react-router-dom";    
+import {NavLink, useNavigate } from "react-router-dom";    
 import { UserRound } from 'lucide-react';
+import { useEffect, useState } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../../backend/firebase";
 
 function Mainheader(){
+
+  const navigate = useNavigate();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    //verifica persistência
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setIsAuthenticated(!!user);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const handleUserClick = () => {
+    if(isAuthenticated){
+      navigate("/user"); // já logado
+    } else {
+      navigate("/login");
+    }
+  }
 return(
     <div>
       <nav className="px-6 py-4 flex justify-between items-center">
         <div className="text-xl font-bold">
             <NavLink to={"/"}>Fichário do Mestre</NavLink> 
         </div>
-      <NavLink to={'/login'}><UserRound size={30}/></NavLink>
+      <button onClick={handleUserClick}>
+        <UserRound size={30}/>
+      </button>
       </nav>
-      
     </div>
 )   
 }
